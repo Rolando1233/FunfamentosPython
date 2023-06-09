67932ff26207e06186c1f6c130631d7787f5dcbc
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2023-06-09 16:20:56 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Python es No tipado
+
+name = "Rolando"
+last_name = "Flores"
+age = 18
+height = 1.68
+is_student = True
+groups = ["3A", "3B", "3C"]
+
+#function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
