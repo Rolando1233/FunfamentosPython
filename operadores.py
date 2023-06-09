48bb718dacd9570e52290d88725fbde894ff19d9
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2023-06-09 16:52:21 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,21 @@
+# operadores matematicos
+# + - * %
+# /Division float
+# // Diviosion entera
+# ** Potencia
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+# Operadores para strings
+# + para concatenar
+# * para replicar
+print("_______" * 4)
+print("Hola " + "Mundo!")
+print("Hola " * 3)
+
+
