5d0335bf1b04f6049a9cc21932fd995f2e9f2ca5
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2023-06-09 16:59:57 -0600

    05 Operadores Booleanos

---
+++ operadores_2.py
@@ -0,0 +1,18 @@
+# Operadores booleanos
+# == (igualdad)
+print(1 == 1)
+# != (distinto)
+print(1 != 1)
+# > (Mayor que)
+print(1 > 1)
+# < (Menor que)
+print(1 < 1)
+# Operadores logicos
+# and
+print(True and True)
+print(True and False)
+# or
+print(True and True)
+print(False and False)
+# not
+print(not True)
