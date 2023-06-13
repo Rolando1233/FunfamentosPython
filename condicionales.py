eb19a232ad7778484dd57abf7f08390ffa2f2938
Rolando1233 <rolandoantonioflorespalafox@gmail.com>
2023-06-13 12:45:12 -0600

    06 Condicionales

---
+++ condicionales.py
@@ -0,0 +1,22 @@
+# if, elif, else
+# if expresion_booleana:
+# bloque de codigo
+
+age = 18
+
+if age >= 18:
+    print("Es mayor de Edad")
+
+if age < 18:
+    print("Es menor de Edad")
+else: 
+    print("Es mayor de Edad")
+
+age = 75
+
+if age < 18:
+    print("Es menor de Edad")
+elif age < 70:
+    print("Es mayor de edad")
+else:
+    print("Es adulto mayor")
